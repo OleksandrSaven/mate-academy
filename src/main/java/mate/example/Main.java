3ac280080376a020b2a3f5c8dd9369024d6cf54a
Oleksandr Savenets <savenvoin@gmail.com>
2023-05-25 15:01:59 +0300

    add hello mates

--- src/main/java/mate/example/Main.java
+++ src/main/java/mate/example/Main.java
@@ -3,5 +3,6 @@ package mate.example;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello World!");
+        System.out.println("Hello mates!");
     }
 }
